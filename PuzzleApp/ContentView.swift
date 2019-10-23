21c1070ec83afbcabc821620c683842ff1fb3744
m4riusz <sariuszm@gmail.com>
2019-10-23 08:51:37 +0200

    Initial Commit

---
+++ PuzzleApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PuzzleApp
+//
+//  Created by Mariusz Sut on 23/10/2019.
+//  Copyright © 2019 Mariusz Sut. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
