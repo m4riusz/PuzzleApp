66ed6e1f66b53cde3b71b03d64584e44f882d99a
m4riusz <mariusz.sut.94+github@gmail.com>
2019-10-23 13:48:40 +0200

    add app icon, cleanup

--- PuzzleApp/Main/MainView.swift
+++ PuzzleApp/Main/MainView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MainView.swift
 //  PuzzleApp
 //
 //  Created by Mariusz Sut on 23/10/2019.
@@ -8,14 +8,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainView: View {
     var body: some View {
-        Text("Hello World")
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct MainView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainView()
     }
 }
