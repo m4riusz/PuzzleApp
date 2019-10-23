60e15e11733e681f65f7edce789e7647a5b199d7
m4riusz <mariusz.sut.94+github@gmail.com>
2019-10-23 19:29:52 +0200

    basic form

---
+++ PuzzleApp/Views/SliderView.swift
@@ -0,0 +1,42 @@
+//
+//  SliderView.swift
+//  PuzzleApp
+//
+//  Created by Mariusz Sut on 23/10/2019.
+//  Copyright © 2019 Mariusz Sut. All rights reserved.
+//
+
+import SwiftUI
+
+struct SliderView: View {
+    @Binding var minValue: Float
+    @Binding var maxValue: Float
+    @Binding var currentValue: Float
+    
+    var body: some View {
+        VStack {
+            Slider(value: $currentValue,
+                   in: minValue...maxValue,
+                   step: 1,
+                   onEditingChanged: { r in },
+                   minimumValueLabel: Text("\(Int(minValue))")
+                    .font(.body),
+                   maximumValueLabel: Text("\(Int(maxValue))")
+                    .font(.body)) {
+                        Text("")
+            }
+            .padding([.top], 5)
+            Text("\(Int(currentValue))")
+                .font(.callout)
+                .fontWeight(.light)
+        }
+    }
+}
+
+struct SliderView_Previews: PreviewProvider {
+    static var previews: some View {
+        SliderView(minValue: Binding<Float>.constant(4),
+                   maxValue: Binding<Float>.constant(10),
+                   currentValue: Binding<Float>.constant(5))
+    }
+}
